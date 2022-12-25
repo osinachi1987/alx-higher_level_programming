8afd0578df290378397d83e9ce832ab5390d6cc8
Festus Osinachi <connect2festus@gmail.com>
2022-12-25 05:54:31 -0800

    All Task

---
+++ 0x07-python-test_driven_development/5-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("""The customer is very important, the customer will be followed by the customer. \
+In what way? So do you think it's literal or does it affect whole pages? \
+But not this: therefore not even that. Maybe that's best, but where is that: \
+Ever more pleasure? I hold, said he, that the end seemed to him no pain. \
+Transfer the same to modesty or temperance, which is the control of desires.
+obeying reason If you say so, we won. Then, in a varied speech, that sex from Dipylus
+We finished the stages. If you want anything else, later. What is the affection of his soul
+giving to each and this, which I say. If only they would say something else
+happier! You could already see the ruins""")
